edefbf570e5308deab0bdcba603ad62d21ffd88d
jmruiz894 <jmruiz0894@gmail.com>
2021-01-24 15:40:04 -0300

    🚧 Creating sequelize config setup

---
+++ commons/sequelize.setup.js
@@ -0,0 +1,13 @@
+/**
+ * Constructor properties of Sequelize instance
+ */
+import path from "path";
+
+export default [
+    {
+        dialect: process.env.SEQUELIZE_DIALECT || "sqlite",
+        storage: 
+        process.env.SEQUELIZE_DIALECT_STORAGE ||
+        path.resolve(__dirname, "dist/db/temporal_db.sqlite"),
+    },
+];
