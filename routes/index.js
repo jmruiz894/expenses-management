af3d828a2c9d0d0b2585a571e76f103df2e47a7d
jmruiz894 <jmruiz0894@gmail.com>
2021-01-24 13:59:55 -0300

    ⚡️ Adding v1 router on api router

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 import { Router } from "express";
+import apiV1 from "./api-v1.router";
 
-const router = Router();
+const router = Router({
+    mergeParams: true,
+});
+
 router.all("/", (req, res) => {
     res.send({ message: "welcome to express server" });
-});
+  });
+router.use("/api", apiV1);
 
 export default router;
