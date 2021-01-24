54b302050edfda2c13c74b780eaf8350375a9dae
jmruiz894 <jmruiz0894@gmail.com>
2021-01-24 14:53:43 -0300

    ✨ Creating database's user model

---
+++ entities/user.entity.js
@@ -0,0 +1,31 @@
+import { Model, DataTypes } from "sequelize";
+import tables from "../references/db-table-name.reference";
+import commmonAttributes from "./entity.common-attributes";
+import commonOptions from "../commons/sequelize-instance.config";
+
+class User extends Model {}
+
+User.init(
+    {
+      ...commonAttributes,
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      // TODO: Include userName property when user authentication has been done
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      ...commonOptions,
+      modelName: tables.USER,
+    }
+  );
+  
+  export default User;
