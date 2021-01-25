26c55d92a33cf9a6245a32a629044e7c96412cfa
jmruiz894 <jmruiz0894@gmail.com>
2021-01-25 10:46:19 -0300

    💄 Updating logging output format

--- core/logger.js
+++ core/logger.js
@@ -6,15 +6,31 @@ const getLogDir = (name) =>
     process.env.LOG_PATH || path.resolve(__dirname, "../dist"),
     `${name}.log`
   );
+const loggerFormat = format.combine(
+  format.align(),
+  format.timestamp(),
+  format.colorize(),
+  format.simple(),
+  format.printf(({ timestamp, level, message }) => {
+    const label = `${timestamp}   ${level}   `;
+    const labelOffset = " ".repeat(label.length - 20); // colorize offset
+    const formattedMessage = message
+      .trim()
+      .match(/.{1,100}/g)
+      .map((line, i) => (i ? labelOffset + line : line))
+      .join("\n");
+    return `${label}${formattedMessage}`;
+  })
+);
 const logger = createLogger({
   level: process.env.LOG_LEVEL || "info",
-  format: format.combine(format.colorize(), format.simple()),
+  format: loggerFormat,
   transports: [new transports.File({ filename: getLogDir("ml-log") })],
 });
 if (JSON.parse(process.env.SHOW_CONSOLE || "1")) {
   logger.add(
     new transports.Console({
-      format: format.simple(),
+      format: loggerFormat,
     })
   );
 }
