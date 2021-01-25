e3ff12eddca68c458b201d644c81099b9db1d774
jmruiz894 <jmruiz0894@gmail.com>
2021-01-25 03:41:27 -0300

    🐎 Updating app initialization to ensure to load db before start server

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-import app from "./server";
+import { app, beforeLoad } from "./server";
 
 const port = process.env.APP_PORT || 3000;
 
-app.listen(port, () => {
+const startLocal = async (appInstance, appPort, beforeLoadFuncs = []) => {
+  await Promise.all(beforeLoadFuncs.map((func) => Promise.resolve(func())));
+  return new Promise((resolve) => {
+    appInstance.listen(appPort, () => resolve(true));
+  });
+};
+
+startLocal(app, port, beforeLoad).then(() => {
   // eslint-disable-next-line no-console
   console.log(`App listening on port ${port}!`);
 });
--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 import express from "express";
 // eslint-disable-next-line import/no-extraneous-dependencies
 import bodyParser from "body-parser";
-import router from "./routes/index";
+import router from "./routes";
+import { initialize } from "./database";
 
-const app = express();
-
+export const app = express();
+export const beforeLoad = [initialize];
 /**
  * Global App Config
  */
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/", router);
-
-export default app;
