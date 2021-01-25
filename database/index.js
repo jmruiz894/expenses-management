01d59f074639fb0f33c6817f3b66497e9909fc99
jmruiz894 <jmruiz0894@gmail.com>
2021-01-25 11:06:43 -0300

    🔨 Replacing database initialization

--- database/index.js
+++ database/index.js
@@ -1,25 +1,11 @@
 import init from "./database.init";
-import TransactionTypeFactory from "./transaction-type.factory";
-import TransactionFactory from "./transaction.factory";
-import UserFactory from "./user.factory";
-import TransactionTypeRepository from "./transaction-type.repository";
-import TransactionRepository from "./transaction.repository";
-import UserRepository from "./user.repository";
 import setup from "../configs/sequelize.setup";
+import DatabaseFactory from "./database.factory";
 
-export const instances = {};
+export const repositories = new DatabaseFactory();
+
 export async function initialize() {
   const db = await init(setup);
-  const User = UserFactory(db);
-  const Transaction = TransactionFactory(db);
-  const TransactionType = TransactionTypeFactory(db);
-  // Defining Table's relationship
-  User.hasMany(Transaction);
-  Transaction.belongsTo(User);
-  Transaction.belongsTo(TransactionType);
-  TransactionType.hasMany(Transaction);
-  instances.userRepository = new UserRepository(User);
-  instances.transactionRepository = new TransactionRepository(Transaction);
-  instances.transactionTypeRepository = new TransactionTypeRepository(User);
+  repositories.setDatabase(db);
   return db;
 }
