704dd3df72993df58c3f12c38967bf3fdfe82d51
jmruiz894 <jmruiz0894@gmail.com>
2021-01-26 00:09:47 -0300

    ✨ Implementing methods on transaction controller

--- controllers/transaction.controller.js
+++ controllers/transaction.controller.js
@@ -1,32 +1,44 @@
+import logger from "../core/logger";
+import { repositories } from "../database";
 import statusCode from "../references/http-status-codes.references";
+import tables from "../references/db-table-name.reference";
 
 class TransactionController {
-  getTransaction(req, res) {
-    return res.status(statusCode.methodNotAllowed).send({
-      message: "Method not implemented yet",
-      query: req.query,
-    });
+  #repository;
+
+  getRepository() {
+    if (!this.#repository)
+      this.#repository = repositories.getInstance(tables.TRANSACTION);
+    return this.#repository;
   }
 
-  createTransaction(req, res) {
-    return res.status(statusCode.methodNotAllowed).send({
-      message: "Method not implemented yet",
-      query: req.query,
-    });
+  async getTransaction(req, res) {
+    const transaction = await this.getRepository().find(req.query);
+    logger.info(`[GET] Transaction data: ${JSON.stringify(transaction)}`);
+    return res.status(statusCode.oK).send(transaction || {});
   }
 
-  updateTransaction(req, res) {
-    return res.status(statusCode.methodNotAllowed).send({
-      message: "Method not implemented yet",
-      query: req.query,
-    });
+  async createTransaction(req, res) {
+    const transaction = await this.getRepository().create(req.body);
+    logger.info(`[POST] Transaction data: ${JSON.stringify(transaction)}`);
+    return res.status(statusCode.oK).send(transaction || {});
   }
 
-  deleteTransaction(req, res) {
-    return res.status(statusCode.methodNotAllowed).send({
-      message: "Method not implemented yet",
-      query: req.query,
-    });
+  async updateTransaction(req, res) {
+    const updatedRows = await this.getRepository().updateById(
+      req.params.transactionId,
+      req.body
+    );
+    logger.info(`[PUT] Transaction data: ${updatedRows}`);
+    return res.status(statusCode.oK).send({ updated: updatedRows });
+  }
+
+  async deleteTransaction(req, res) {
+    const updatedRows = await this.getRepository().deleteById(
+      req.params.transactionId
+    );
+    logger.info(`[DEL] Transaction data: ${updatedRows}`);
+    return res.status(statusCode.oK).send({ deleted: updatedRows });
   }
 }
 
